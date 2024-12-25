2e153b66716fe2a1d4db9c943b4e58c70593140b
NavdeepKakrod <150247209+NavdeepKakrod@users.noreply.github.com>
2024-12-25 12:54:35 +0530

    Update Weather.js
    Fix: Update Weather.js to use OpenWeatherMap API

--- Weather.js
+++ Weather.js
@@ -1,51 +1,49 @@
 function getfunc() {
+    const city = Search.value;
+    const apiKey = 'ccfd6fa99c21da1d2631bc0af235d447';  
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-    const city=Search.value;
-    const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city;
-    const options = {
-        method: 'GET',
-        headers: {
-            'x-rapidapi-key': 'ee3254b947msh057d092ac9c5c00p172ff1jsnc5bf0202f02c',
-            'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
-        }
-    };
-    
-    fetch(url, options)
-        .then(response => response.json())
-        .then(res=>{
-            console.log(res);
-            temp.innerHTML = res.temp;
-            hmdt.innerHTML = res.humidity;
-            ws.innerHTML = res.wind_speed;
-            feellike.innerHTML = res.feels_like;
-            if(res.cloud_pct>70){
-                document.body.style.backgroundImage = "url('Images/Rainy.gif')";
-                chances.innerHTML="Rain Possible";
-                
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`City not found (${response.status})`);
             }
-            else{
-            if (res.temp>33) {
-                document.body.style.backgroundImage = "url('Images/summer.gif')";   
-                chances.innerHTML="It's Hot Outside"; 
-            
-                        
-            }
-            else if(res.temp>15){
-                document.body.style.backgroundImage = "url('Images/normal.gif')";
-                chances.innerHTML="It's Nice Outside";
-
-            }
-            else{
-                document.body.style.backgroundImage = "url('Images/winter.gif')";
-                chances.innerHTML="Cool Outside";
-
-            }
-            }
-
+            return response.json();
         })
-        .catch(err => console.error(err));
-}
+        .then(res => {
+            console.log(res);
+            const temp = res.main.temp;
+            const humidity = res.main.humidity;
+            const windSpeed = res.wind.speed;
+            const feelsLike = res.main.feels_like;
+            const cloudPct = res.clouds.all;
 
+            document.getElementById('temp').innerHTML = temp;
+            document.getElementById('hmdt').innerHTML = humidity;
+            document.getElementById('ws').innerHTML = windSpeed;
+            document.getElementById('feellike').innerHTML = feelsLike;
+
+            if (cloudPct > 70) {
+                document.body.style.backgroundImage = "url('Images/Rainy.gif')";
+                document.getElementById('chances').innerHTML = "Rain Possible";
+            } else {
+                if (temp > 33) {
+                    document.body.style.backgroundImage = "url('Images/summer.gif')";
+                    document.getElementById('chances').innerHTML = "It's Hot Outside";
+                } else if (temp > 15) {
+                    document.body.style.backgroundImage = "url('Images/normal.gif')";
+                    document.getElementById('chances').innerHTML = "It's Nice Outside";
+                } else {
+                    document.body.style.backgroundImage = "url('Images/winter.gif')";
+                    document.getElementById('chances').innerHTML = "Cool Outside";
+                }
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            alert('Error: Unable to fetch weather data. Please check the city name.');
+        });
+}
 function THEME(seson){
     if (seson =="Rainy") {
         document.body.style.backgroundImage = "url('Images/Rainy.gif')";
